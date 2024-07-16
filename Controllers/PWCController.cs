f0775f3bdd5cbfeec01b38ccd818b29f6647cc16
qinghong <qinghong.work@gmail.com>
2024-07-16 17:43:45 +0800

    Transferrred Problems W1D1~W1D6

--- Controllers/PWCController.cs
+++ Controllers/PWCController.cs
@@ -8,5 +8,29 @@ namespace Qing_Hong_Blog.Controllers
         {
             return View();
         }
+        public IActionResult W1D2()
+        {
+            return View();
+        }
+        public IActionResult W1D3()
+        {
+            return View();
+        }
+        public IActionResult W1D4()
+        {
+            return View();
+        }
+        public IActionResult W1D5()
+        {
+            return View();
+        }
+        public IActionResult W1D6()
+        {
+            return View();
+        }
+        public IActionResult W1D7()
+        {
+            return View();
+        }
     }
 }
