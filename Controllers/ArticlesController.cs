d1382ba9d09ec7ee817d7d65ae4fd1a9ba8b5075
qinghong <qinghong.work@gmail.com>
2024-07-18 17:30:05 +0800

    First article.

---
+++ Controllers/ArticlesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Qing_Hong_Blog.Controllers
+{
+    public class ArticlesController : Controller
+    {
+        public IActionResult index()
+        {
+            return View();
+        }
+
+        public IActionResult att()
+        {
+            return View();
+        }
+    }
+}
